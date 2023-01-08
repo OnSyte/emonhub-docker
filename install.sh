5afd942a578670ce821bf33eff1585f816ba2dc1
George Barlow <george@barlw.uk>
2023-01-08 17:04:01 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,35 +11,35 @@ echo "EmonHub directory: $script_dir"
 
 # User input: check username to install emonhub with
 echo "Running apt update"
-sudo apt update
+apt update
 
 echo "installing or updating emonhub dependencies"
-sudo apt-get install -y python3-serial python3-configobj python3-pip python3-pymodbus bluetooth libbluetooth-dev python3-spidev
+apt-get install -y python3-serial python3-configobj python3-pip python3-pymodbus bluetooth libbluetooth-dev python3-spidev
 pip3 install paho-mqtt requests pybluez py-sds011 sdm_modbus minimalmodbus
 
 # Custom rpi-rfm69 library used for SPI RFM69 Low Power Labs interfacer
 pip3 install https://github.com/openenergymonitor/rpi-rfm69/archive/refs/tags/v0.3.0-oem-4.zip
 
 # this should not be needed on main user but could be re-enabled
-# sudo useradd -M -r -G dialout,tty -c "emonHub user" emonhub
+# useradd -M -r -G dialout,tty -c "emonHub user" emonhub
 
 # ---------------------------------------------------------
 # EmonHub config file
 # ---------------------------------------------------------
 if [ ! -d /etc/emonhub ]; then
     echo "Creating /etc/emonhub directory"
-    sudo mkdir /etc/emonhub
-    sudo mkdir /var/log/emonhub
+    mkdir /etc/emonhub
+    mkdir /var/log/emonhub
 else
     echo "/etc/emonhub directory already exists"
 fi
 
 if [ ! -f /etc/emonhub/emonhub.conf ]; then
-    sudo cp $script_dir/conf/emonpi.default.emonhub.conf /etc/emonhub/emonhub.conf
+    cp $script_dir/conf/emonpi.default.emonhub.conf /etc/emonhub/emonhub.conf
     echo "No existing emonhub.conf configuration file found, installing default"
     
     # requires write permission for configuration from emoncms:config module
-    sudo chmod 666 /etc/emonhub/emonhub.conf
+    chmod 666 /etc/emonhub/emonhub.conf
     echo "emonhub.conf permissions adjusted to 666"
 
     # Temporary: replace with update to default settings file
@@ -50,11 +50,11 @@ fi
 # Fix emonhub log file permissions
 if [ -d /var/log/emonhub ]; then
     echo "Setting ownership of /var/log/emonhub to $user"
-    sudo chown $user /var/log/emonhub
+    chown $user /var/log/emonhub
 fi
 
 if [ -f /var/log/emonhub/emonhub.log ]; then
     echo "Setting ownership of /var/log/emonhub/emonhub.log to $user and permissions to 644"
-    sudo chown $user:$user /var/log/emonhub/emonhub.log
-    sudo chmod 644 /var/log/emonhub/emonhub.log
+    chown $user:$user /var/log/emonhub/emonhub.log
+    chmod 644 /var/log/emonhub/emonhub.log
 fi
